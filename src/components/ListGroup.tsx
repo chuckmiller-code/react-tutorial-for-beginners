e2e34e1c7384546cda2a10ce6cbff75a2fc4f04a
Chuck Miller <82245768+thechuckmiller@users.noreply.github.com>
2024-08-17 22:02:24 -0500

    Third commit

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,15 +1,18 @@
+
 function ListGroup() {
+  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  let selectedIndex = 0;
+
   return (
     <>
-        <>List</>
-        <ul className="list-group">
-          <li className="list-group-item">An item</li>
-          <li className="list-group-item">A second item</li>
-          <li className="list-group-item">A third item</li>
-          <li className="list-group-item">A fourth item</li>
-          <li className="list-group-item">And a fifth one</li>
-        </ul>
-    </>
+      <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li className={selectedIndex === index ? 'list-group-item active' : 'list-group-item'} onClick={handleClick} key={item}>{item}</li>
+        ))}
+      </ul>
+    </> 
   );
 }
 
